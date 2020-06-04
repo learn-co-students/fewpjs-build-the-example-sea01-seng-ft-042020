690d3272ddc44843a98bb61feac45d555d808590
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-03 21:38:19 -0700

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function() {
+  // Toggle Page Error
+  function togglePageError() {
+    const pageError = document.getElementById('modal')
+    pageError.classList.remove('hidden')
+    setTimeout(function() {pageError.setAttribute('class','hidden')}, 5000)
+  }
 
+  // Toggle Like Button via Event Listener
+  function toggleLike(likeButton) {
+    likeButton.addEventListener('click', function(event) {
+      if (event.target.innerText === EMPTY_HEART) {
+        mimicServerCall()
+        .then(function() {
+          event.target.innerHTML = FULL_HEART
+          event.target.classList.add('activated-heart')})
+        .catch(function(error) {
+          togglePageError()})
+      } else {
+        event.target.innerHTML = EMPTY_HEART
+        event.target.classList.remove('activated-heart')
+      }
+    })
+  }
 
-
+  // Add Event Listener on All Like Buttons
+  const likeButtons = document.getElementsByClassName('like-glyph')
+  for (const likeButton of likeButtons) {
+    toggleLike(likeButton)
+  }
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
